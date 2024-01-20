ac7b4f86bbd96e570a4c3ed72c97e92c21234d7a
Dale-jpg <echols.dale@gmail.com>
2024-01-20 16:23:23 -0500

    First test passed.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function removeFromArray(array, indexArray) {
+    const index = array.indexOf(indexArray);
+    if (index > -1) {
+        array.splice(index,1);
+    }
+    return array;
 };
 
 // Do not edit below this line
